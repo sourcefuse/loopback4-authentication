83bb000631b88a8de5ea012010326986b761c9fb
Ankur Bansal <85155003+AnkurBansalSF@users.noreply.github.com>
2022-11-21 19:17:50 +0530

    refactor(component): remove duplicate code block from action and middleware providers (#106)
    remove duplicate code block from action and middleware providers to a new helper provider
    
    gh-105

--- src/middlewares/client-authentication.middleware.ts
+++ src/middlewares/client-authentication.middleware.ts
@@ -1,23 +1,12 @@
-import {
-  Getter,
-  inject,
-  injectable,
-  Next,
-  Provider,
-  Setter,
-} from '@loopback/core';
+import {inject, injectable, Next, Provider} from '@loopback/core';
 import {
   asMiddleware,
   Middleware,
   MiddlewareContext,
   RestMiddlewareGroups,
-  Request,
 } from '@loopback/rest';
-import {Strategy} from 'passport';
 import {AuthenticationBindings} from '../keys';
-import {StrategyAdapter} from '../strategy-adapter';
-import {IAuthClient} from '../types';
-import {isObjectLike, isEmpty} from 'lodash';
+import {AuthenticateFn, IAuthClient} from '../types';
 import {AuthenticationMiddlewareGroups} from './middleware-groups.enum';
 
 @injectable(
@@ -34,37 +23,15 @@ export class ClientAuthenticationMiddlewareProvider
   implements Provider<Middleware>
 {
   constructor(
-    @inject.getter(AuthenticationBindings.CLIENT_STRATEGY)
-    readonly getStrategy: Getter<Strategy>,
-    @inject.setter(AuthenticationBindings.CURRENT_CLIENT)
-    readonly setCurrentClient: Setter<IAuthClient | undefined>,
+    @inject(AuthenticationBindings.CLIENT_AUTH_ACTION)
+    private authenticateClient: AuthenticateFn<IAuthClient | undefined>,
   ) {}
 
   value() {
     const middleware = async (ctx: MiddlewareContext, next: Next) => {
-      await this.action(ctx.request);
+      await this.authenticateClient(ctx.request);
       return next();
     };
     return middleware;
-  }
-  async action(request: Request): Promise<IAuthClient | undefined> {
-    const strategy = await this.getStrategy();
-    if (!strategy) {
-      this.setCurrentClient(undefined);
-      return undefined;
-    }
-    if (!strategy.authenticate) {
-      throw new Error('invalid strategy parameter');
-    }
-    const strategyAdapter = new StrategyAdapter<IAuthClient>(strategy);
-    // Added for cases, where data is passed not in body but in query parameter
-    if (!request.body || !isObjectLike(request.body) || isEmpty(request.body)) {
-      request.body = request.query;
-    }
-    const client = await strategyAdapter.authenticate(request);
-    if (client) {
-      this.setCurrentClient(client);
-      return client;
-    }
   }
 }
--- src/middlewares/user-authentication.middleware.ts
+++ src/middlewares/user-authentication.middleware.ts
@@ -1,25 +1,12 @@
-import {
-  Getter,
-  inject,
-  injectable,
-  Next,
-  Provider,
-  Setter,
-} from '@loopback/core';
+import {inject, injectable, Next, Provider} from '@loopback/core';
 import {
   asMiddleware,
-  HttpErrors,
   Middleware,
-  Response,
   MiddlewareContext,
   RestMiddlewareGroups,
-  Request,
 } from '@loopback/rest';
-import {Strategy} from 'passport';
-import {AuthErrorKeys} from '../error-keys';
 import {AuthenticationBindings} from '../keys';
-import {StrategyAdapter} from '../strategy-adapter';
-import {AuthenticationMetadata, IAuthUser} from '../types';
+import {AuthenticateFn, IAuthUser} from '../types';
 import {AuthenticationMiddlewareGroups} from './middleware-groups.enum';
 
 @injectable(
@@ -33,56 +20,15 @@ export class UserAuthenticationMiddlewareProvider
   implements Provider<Middleware>
 {
   constructor(
-    @inject.getter(AuthenticationBindings.USER_STRATEGY)
-    readonly getStrategy: Getter<Strategy>,
-    @inject.getter(AuthenticationBindings.USER_METADATA)
-    private readonly getMetadata: Getter<AuthenticationMetadata>,
-    @inject.setter(AuthenticationBindings.CURRENT_USER)
-    readonly setCurrentUser: Setter<IAuthUser | undefined>,
+    @inject(AuthenticationBindings.USER_AUTH_ACTION)
+    private authenticateUser: AuthenticateFn<IAuthUser | undefined>,
   ) {}
 
   value() {
     const middleware = async (ctx: MiddlewareContext, next: Next) => {
-      await this.action(ctx.request, ctx.response);
+      await this.authenticateUser(ctx.request, ctx.response);
       return next();
     };
     return middleware;
-  }
-
-  async action(
-    request: Request,
-    response?: Response,
-  ): Promise<IAuthUser | undefined> {
-    const strategy = await this.getStrategy();
-
-    if (!strategy) {
-      this.setCurrentUser(undefined);
-      return undefined;
-    }
-    if (!strategy.authenticate) {
-      throw new HttpErrors.Unauthorized(AuthErrorKeys.UnknownError);
-    }
-
-    // Read decorator metadata to fetch options
-    // to be passed on to authenticate method of strategy
-    const metadata = await this.getMetadata();
-    let authOpts;
-    if (metadata?.authOptions) {
-      // Fetch options using creator function added with decorator definition
-      authOpts = metadata.authOptions(request);
-    }
-
-    const strategyAdapter = new StrategyAdapter<IAuthUser>(strategy);
-
-    const user = await strategyAdapter.authenticate(
-      request,
-      response,
-      authOpts,
-    );
-
-    if (user) {
-      this.setCurrentUser(user);
-      return user;
-    }
   }
 }
